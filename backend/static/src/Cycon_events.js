670b008f175d31ddcd3b180acebc0cfe9ff90d22
Amin Mirkouei <amirkouei@uidaho.edu>
2024-04-24 14:09:19 -0600

    fix: date

--- backend/static/src/Cycon_events.js
+++ backend/static/src/Cycon_events.js
@@ -1996,7 +1996,7 @@ function generatePDF(form) {
           pdf.setFontSize(10);
           pdf.setTextColor(150);
           // Add your footer content here
-          pdf.text("Copyright © CyCon 2024 version. Last updated: 04/17/2024", pdf.internal.pageSize.getWidth() - 100, pdf.internal.pageSize.getHeight() - 10);
+          pdf.text("Copyright © CyCon 2024 version. Last updated: 04/24/2024", pdf.internal.pageSize.getWidth() - 100, pdf.internal.pageSize.getHeight() - 10);
         }
   
         // Save the PDF with the footer
